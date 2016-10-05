493f3c7b98d54b5daf25a3bcb084ea7fda7a0853
ZMarushchak <zakhar.marushchak@gmail.com>
2016-10-05 19:36:27 +0300

    python test app commit

---
+++ app/test.py
@@ -0,0 +1,17 @@
+import unittest
+from subprocess import check_output
+import os
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+class test(unittest.TestCase):
+
+    def test(self):
+        # print "CURRENT DIR::::%s" % CURRENT_DIR
+        out = check_output(["python", CURRENT_DIR + "/app.py"])
+        # print "PRINTED:%s" % out
+        return self.assertTrue(out.find('Hello')!=-1)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(test)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
