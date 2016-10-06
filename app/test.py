1c571dfd1e92fcb1a3f3e0be08709ae220ab02c2
ZMarushchak <zakhar.marushchak@gmail.com>
2016-10-06 16:02:05 +0300

    a

--- app/test.py
+++ app/test.py
@@ -7,10 +7,10 @@ CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 class test(unittest.TestCase):
 
     def test(self):
-        print "CURRENT DIR::::%s" % CURRENT_DIR
+        #print "CURRENT DIR::::%s" % CURRENT_DIR
         out = check_output(["python", CURRENT_DIR + "/app.py"])
         # print "PRINTED:%s" % out
-        return self.assertTrue(out.find('Hello')!=-1)
+        return self.assertTrue(out.find('FUCK')!=-1)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(test)
 unittest.TextTestRunner(verbosity=2).run(suite)
