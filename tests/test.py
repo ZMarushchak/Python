cf4df1389130d29bc0a67860507b8048c8ac0be3
root <root@t540p-zakhar.lviv.intelliarts.com>
2016-10-11 12:54:10 +0300

    selenium tests adding

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,30 @@
 import unittest
-from subprocess import check_output
+from selenium import webdriver
 import os
 import sys
+import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class test(unittest.TestCase):
 
     def test(self):
+        while True:
+            try:
+                self.chrome = webdriver.Remote(command_executor='http://hub:4444/wd/hub',
+                                       desired_capabilities=DesiredCapabilities.CHROME)
+                self.firefox = webdriver.Remote(command_executor='http://hub:4444/wd/hub',
+                                       desired_capabilities=DesiredCapabilities.FIREFOX)
+                break
+            except:
+                pass
         #print "CURRENT DIR::::%s" % CURRENT_DIR
-        out = check_output(["python", CURRENT_DIR + "/app.py"])
         # print "PRINTED:%s" % out
-        return self.assertTrue(out.find('Hello')!=-1)
+        self.firefox.get('http://app:5000')
+        body = self.firefox.find_element_by_css_selector('body')
+        return self.assertIn('hello', body.text)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(test)
 runner = unittest.TextTestRunner(verbosity=2).run(suite)
